f82c5b1987b236f37961d0fc72efc4b7c74a68ea
Vinicius Barbosa de Medeiros <bmvinicius11@gmail.com>
2018-03-16 12:56:12 -0300

    Java 1002, .gitignore files

---
+++ Java/1002/Main.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		double PI = 3.14159;
+
+		Scanner sc = new Scanner(System.in);
+		double radius;
+		
+		radius = sc.nextDouble();
+		sc.close();
+		double area = PI * (radius * radius);
+
+		System.out.printf("A=%.4f\n",area);
+
+	}
+
+}
