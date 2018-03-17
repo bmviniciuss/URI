1c9ef812c1b04ce5ae0824659c32dae61fdd4c3d
Vinicius Barbosa de Medeiros <bmvinicius11@gmail.com>
2018-03-17 19:21:38 -0300

    Java - 1007

---
+++ Java/1007/Main.java
@@ -0,0 +1,26 @@
+
+// 1007 - DIFERENÇA
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    int a, b, c, d,diference;
+
+    Scanner sc = new Scanner(System.in);
+
+    b = sc.nextInt();
+    a = sc.nextInt();
+    c = sc.nextInt();
+    d = sc.nextInt();
+    sc.close();
+
+    diference = ((a * b) - (c * d));
+
+    System.out.printf("DIFERENCA = %d\n", diference);
+
+  }
+
+}
